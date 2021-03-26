13bc1f0767885c696f25d9ca73616869fbf487a5
Osmar da Cunha Filho <Osmar da Cunha Filho>
2021-03-26 20:20:42 -0300

    ed. 2

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <img src= "https://i.pinimg.com/originals/e5/33/da/e533dadea572d5a85ddfe5bb64eea8e2.gif" alt=""/>
         <p>
         Olá Mundo! Edição do Prof. Osmar.
+        Edição n. 2.
         </p>
       
     </div>
