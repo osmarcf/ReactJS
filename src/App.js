f7462a111d49adc83e2ba91bac7babb1b396eba2
david da silva <david_tiopatinhas@hotmail.com>
2021-03-26 20:12:01 -0300

    Ola Mundo

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <img src= "https://i.pinimg.com/originals/e5/33/da/e533dadea572d5a85ddfe5bb64eea8e2.gif" alt=""/>
+        <p>
+         Olá Mundo!
+        </p>
+      
+    </div>
+
+  );
+}
+
+export default App;
